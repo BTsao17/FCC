0b9100196edc59fde17ab61e71f4014f30b58052
Bonnie Tsao <IceJade1788@gmail.com>
2020-08-13 23:33:22 -0700

    landing page code from CodePen acct

---
+++ web_design/landing_page/public/script.js
@@ -0,0 +1,9 @@
+function openDropdown() {
+  const overlay = document.getElementById('overlay');
+  overlay.style.width = '100%';
+}
+
+function closeDropdown() {
+  const overlay = document.getElementById('overlay');
+  overlay.style.width = '0%';
+}
